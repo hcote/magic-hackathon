844133f03ea5c8f36c2301e11c32aff90398f2ae
Hunter Cote <huntercote2@gmail.com>
2020-12-11 16:46:12 -0500

    adding try catch to prevent 500 on server for nextjs. deleted unnecessary images

--- templates/nextjs-withsocial/pages/api/user.js
+++ templates/nextjs-withsocial/pages/api/user.js
@@ -1,6 +1,10 @@
 import { getSession } from '../../lib/iron';
 
 export default async function user(req, res) {
-  const session = await getSession(req);
-  res.status(200).json({ user: session || null });
+  try {
+    const session = await getSession(req);
+    res.status(200).json({ user: session || null });
+  } catch (error) {
+    res.status(200).json({ user: null });
+  }
 }
