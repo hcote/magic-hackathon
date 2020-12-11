acb9a10146916239fc6161a03e16561437cb91ca
Ian K Smith <smithki707@gmail.com>
2020-12-10 18:36:10 -0800

    Add more flexibilty to the generator

--- src/generator.ts
+++ src/generator.ts
@@ -7,51 +7,62 @@ const generator = zombi({
 });
 
 generator
-  .prompt<{ template: string; bar: string }>([
+  .prompt<{ framework: string; database: string; socialLogins: string[] }>([
     {
-      name: 'template',
-      message: 'Choose a full-stack template:',
+      name: 'framework',
+      message: 'Choose a full-stack framework:',
       type: 'AutoComplete',
       choices: [
         { name: 'express', message: 'Express' },
-        { name: 'express-fauna', message: 'Express + FaunaDB' },
-        { name: 'express-typescript', message: 'Express + TypeScript' },
-
         { name: 'nextjs', message: 'NextJS' },
-        { name: 'nextjs-fauna', message: 'NextJS + FaunaDB' },
-        { name: 'nextjs-typescript', message: 'NextJS + TypeScript' },
+      ],
+    },
 
-        { name: 'react', message: 'Vanilla React' },
-        { name: 'react-typescript', message: 'Vanilla React + TypeScript' },
+    {
+      name: 'database',
+      message: 'Choose a database:',
+      type: 'Select',
+      choices: [
+        { name: 'none', message: 'None' },
+        { name: 'fauna', message: 'FaunaDB' },
+      ],
+    },
+
+    {
+      name: 'socialLogins',
+      message: 'Choose social login providers:',
+      type: 'MultiSelect',
+      choices: [
+        { name: 'google', message: 'Sign in with Google' },
+        { name: 'facebook', message: 'Sign in with Facebook' },
+        { name: 'github', message: 'Sign in with GitHub' },
       ],
     },
   ])
-  .prompt<{ magicPublicKey: string; magicSecretKey: string; faunaSecret?: string }>(
-    ({ props }) =>
-      [
-        {
-          name: 'magicPublicKey',
-          message: `Paste your Magic public API key:`,
-          type: 'Input',
-        },
+  .prompt<{ magicPublicKey: string; magicSecretKey: string; faunaSecret?: string }>(({ props }) => [
+    {
+      name: 'magicPublicKey',
+      message: `Paste your Magic public API key:`,
+      type: 'Input',
+    },
 
-        {
-          name: 'magicSecretKey',
-          message: `Paste your Magic secret API key:`,
-          type: 'Password',
-        },
+    {
+      name: 'magicSecretKey',
+      message: `Paste your Magic secret API key:`,
+      type: 'Password',
+    },
 
-        props.template.includes('fauna') && {
-          name: 'faunaSecret',
-          message: `Paste your FaunaDB secret:`,
-          type: 'Password',
-        },
-      ].filter(Boolean) as any,
-  )
+    props.database.includes('fauna') && {
+      name: 'faunaSecret',
+      message: `Paste your FaunaDB secret:`,
+      type: 'Password',
+    },
+  ])
   .sequence(
     copy(
-      ({ props }) => props.template,
-      ({ props }) => `${props.template}-magic-app`,
+      ({ props }) =>
+        [props.framework, props.database, props.socialLogins.length && 'withsocial'].filter(Boolean).join('-'),
+      'my-magic-app',
     ),
   )
   .run();
